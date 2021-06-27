c9b8065f960c15ed189f937e61a25e2dab2a15b3
shogo-t <shogo@chalkboard.me>
2021-06-27 23:41:57 +0900

    動的要素の追加

--- components/search.tsx
+++ components/search.tsx
@@ -1,15 +1,91 @@
+import React, { useState } from 'react';
 import styles from '../styles/Search.module.scss'
+
 export default function Search() {
+    // サーチウィンドウの選択状態
+    const [windowActive, setWindowActive] = useState(false)
+    const overMouse = (event: React.MouseEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        setWindowActive(true)
+    }
+    const leaveMouse = (event: React.MouseEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        setWindowActive(false)
+    }
+    /**
+     * windowActiveStatus=trueのとき、引数のclassNameに.activeを追加して返す
+     * @param className
+     * @returns className + .active
+     */
+    const setActiveStatus = (className: string) => {
+        if(windowActive === false) {
+            return className;
+        }
+        return `${className} ${styles.active}`
+    }
+
+    // サーチウィンドウの表示状態
+    const [windowVisble, setWindowVisible] = useState(true)
+
+    // サーチ, ポストのトグルスイッチ管理
+    const [selectSearchTab, setSelectSearchTab] = useState(true)
+    const clickSearch = (event: React.MouseEvent<HTMLLIElement>) => {
+        event.preventDefault();
+        setSelectSearchTab(true)
+    }
+    const clickPost = (event: React.MouseEvent<HTMLLIElement>) => {
+        event.preventDefault();
+        setSelectSearchTab(false)
+    }
+
+    const TabName = {
+        SEARCH: 'search',
+        POST: 'post'
+    } as const;
+    type TabName = typeof TabName[keyof typeof TabName];
+
+    /**
+     * selectSearchTabの値を見て、selectクラスの付与をトグルする
+     * windowActiveの値を見て、activeクラスの付与をトグルする
+     * @param className 
+     * @returns 
+     */
+    const setTabClassName = (tabName:TabName, className: string) => {
+        let name = ''
+        if(windowActive === true) {
+            name = styles.active
+        }
+
+        if(tabName === TabName.SEARCH) {
+            if(selectSearchTab) {
+                name = `${name} ${styles.select}`
+            }
+        } else {
+            if(!selectSearchTab) {
+                name = `${name} ${styles.select}`
+            }
+        }
+
+        return `${className} ${name}`
+    }
+
     return (
-        <div className={styles.container}>
-            <ul className={styles.tab}>
-                <li className={`${styles.menu} ${styles.active}`}>search</li>
-                <li className={styles.menu}>post</li>
+        <div className={styles.container}
+            onMouseEnter={overMouse}
+            onMouseLeave={leaveMouse}
+        >
+            <ul className={setActiveStatus(styles.tab)}>
+                <li className={setTabClassName(TabName.SEARCH, styles.menu)}
+                    onClick={clickSearch}
+                >search</li>
+                <li className={setTabClassName(TabName.POST, styles.menu)}
+                    onClick={clickPost}
+                >post</li>
                 <div className={styles.control}>
                     <button>-</button>
                 </div>
             </ul>
-            <div className={styles.window}>
+            <div className={setActiveStatus(styles.window)}>
                 <div className={styles.item}>
                     <label>Game</label>
                     <select>
