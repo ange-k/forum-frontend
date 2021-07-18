3afa98c3ffca8c7c4085ceb77f32431da774a51b
shogo-t <shogo@chalkboard.me>
2021-07-18 18:24:10 +0900

    selectTagsの分離

--- components/post.tsx
+++ components/post.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import { Game } from '../lib/gen/models/Game';
-import { PostPurposeEnum, PostTagsEnum, PostVcUseEnum } from '../lib/gen/models/Post';
-import { convertTags, proposes, tags, vcuses } from '../lib/helper/genHelper';
+import { PostPurposeEnum, PostVcUseEnum } from '../lib/gen/models/Post';
+import { proposes, vcuses } from '../lib/helper/genHelper';
 import styles from '../styles/PostTab.module.scss'
 
 import ReactTooltip from 'react-tooltip'
 import React from 'react';
+import SelectTags from './selectTags';
 
 type postProps = ({
     games: Game[],
@@ -21,6 +22,7 @@ export interface PostQuery {
     device: string,
     comment: string,
     tags: string[],
+    selfTags: string[],
     deleteKey: string,
     userData?: {
         ipAddr: string,
@@ -29,9 +31,6 @@ export interface PostQuery {
 }
 
 const PostPage:React.FC<postProps> = ({games}) => {
-
-    // Tagの付与は5個まで
-    const [disableTagSelect, setDisableTagSelect] = useState(false)
 
     const [query, setQuery] = useState({
         gameId: 'genshin',
@@ -43,31 +42,9 @@ const PostPage:React.FC<postProps> = ({games}) => {
         device: '',
         comment: '',
         tags: [],
+        selfTags: [],
         deleteKey: ''
     } as PostQuery)
-
-    // 検索タグの設定
-    const tagSelect = ((tagId:string) => {
-        if(!tagId) {
-            return;
-        }
-        const include = query.tags?.includes(tagId)
-        if(query.tags.length > 4) {
-            setDisableTagSelect(true);
-            return
-        }
-        if(!include) {
-            setQuery(query => ({...query, tags: [...query.tags, tagId]}))
-            if(query.tags.length === 4) {
-                setDisableTagSelect(true);
-            }
-            return
-        }
-    })
-    const tagDelete = ((tagId:string) => {
-        setQuery(query => ({...query, tags: query.tags.filter((t) => t !== tagId)}))
-        setDisableTagSelect(false);
-    })
 
     const postApi = (query: PostQuery) => {
         const xhr = new XMLHttpRequest();
@@ -215,31 +192,23 @@ const PostPage:React.FC<postProps> = ({games}) => {
                     </select>
                 </div>
                 <hr className={styles.hr}/>
-                <div className={styles.item}>
-                    <div className={styles.label} data-multiline="true" data-tip="最大5つのタグを紐付ける<br/>ことができます。">
-                        {isTooltipVisible && <ReactTooltip effect="float" type="info"/>}
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"/></svg>
-                        <span className={styles.label}>タグ選択</span>
-                    </div>
-                    <select disabled={disableTagSelect} onChange={(e) => tagSelect(e.target.value)}>
-                        <option value="">未選択</option>
-                        {
-                            tags().map((v) => (
-                                <option key={v.key} value={v.key}>{v.value}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                <div className={styles.tags}>
-                    {
-                        query.tags.map((v) =>(
-                            <button key={v} onClick={(e)=> tagDelete(v)} className={styles.minibtn}>
-                                <div>{convertTags(v as PostTagsEnum)}</div>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
-                            </button>
-                        ))
-                    }
-                </div>
+
+                <SelectTags title="タグ選択(相手に求めること)" selected={query.tags} 
+                    selectTag={(tagId:string)=> {
+                        setQuery(query => ({...query, tags: [...query.tags, tagId]}))
+                    }}
+                    deleteTag={(tagId:string)=> {
+                        setQuery(query => ({...query, tags: query.tags.filter((t) => t !== tagId)}))
+                    }}/>
+                <hr className={styles.hr}/>
+                <SelectTags title="タグ選択(自分に当てはまること)" selected={query.selfTags} 
+                    selectTag={(tagId:string)=> {
+                        setQuery(query => ({...query, selfTags: [...query.selfTags, tagId]}))
+                    }}
+                    deleteTag={(tagId:string)=> {
+                        setQuery(query => ({...query, selfTags: query.selfTags.filter((t) => t !== tagId)}))
+                    }}/>
+
                 <hr className={styles.hr}/>
                 <div className={styles.item}>
                     <span className={styles.label}>
