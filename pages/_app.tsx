1cd36a9d422307d4db75f0a3c163983903db1c33
shogo-t <shogo@chalkboard.me>
2021-07-23 10:29:23 +0900

    document.tsxへ委譲

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,14 @@ import type { AppProps } from 'next/app'
 import HeaderComponent from '../components/header'
 
 import Layout from '../components/layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+      </Head>
       <HeaderComponent />
       <Component {...pageProps} />
     </Layout>
